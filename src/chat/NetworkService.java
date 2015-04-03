748f50d6d42e535d3628d83d69cfbe732a650e09
digitex3d <digitex3d@gmail.com>
2015-04-03 09:28:38 +0200

    Nettoyage du code.

--- src/chat/NetworkService.java
+++ src/chat/NetworkService.java
@@ -8,57 +8,67 @@ import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 class NetworkService implements Runnable {
-	   private final ServerSocket serverSocket;
-	   private final ExecutorService pool;
-	   Dispatcher dispatcher = new Dispatcher();
-	   
-	   // Server logger
-	   private final static Logger LOGGER = 
-			   Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	private ServerSocket serverSocket;
+	private final ExecutorService pool;
+	private int port;
+	Dispatcher dispatcher = new Dispatcher();
+
+	// Server logger
+	private final static Logger LOGGER = 
+			Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 
-	   public NetworkService(int port, int poolSize)
-	       throws IOException {
-	     serverSocket = new ServerSocket(port);
-	     pool = Executors.newFixedThreadPool(poolSize);
-	     LOGGER.info("Network Service Initialised.");
-	     
-	   }
+	public NetworkService(int port, int poolSize){
+		this.port = port;
+		try {
+			this.serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			LOGGER.severe("Cannot open server socket on port " + port);
 
-	   public void run() { 
-		   // run the service
-	     try {
-	       for (;;) {
-	    	   // Client socket
-	    	   Socket cl = serverSocket.accept();
-	    	   LOGGER.info("Ceonnection accepted from " + 
-	    			   				cl.getInetAddress() +
-	    			   				"on port" +
-	    			   				cl.getPort());
-	    	   
-	    	   dispatcher.addClient(cl);
-	    	   pool.execute(new EchoThread(cl, dispatcher));
-	    	   LOGGER.finest("Thread added to ThreadPool");
-	 
-	       }
-	     } catch (IOException ex) {
-	       pool.shutdown();
-	       
-	     }
-	   }
-	   
-	   public void shutDown(){
-		   LOGGER.info("Server is shutting down");
-		   LOGGER.finest("Shutting down ThreadPool");
-		   pool.shutdown();
-		   LOGGER.finest("Shutting down ServerSocket");
-		   try {
+		}
+
+		pool = Executors.newFixedThreadPool(poolSize);
+		LOGGER.info("Network Service Initialised.");
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void run() { 
+		// run the service
+		try {
+			for (;;) {
+				// Client socket
+				Socket cl = serverSocket.accept();
+				LOGGER.info("Ceonnection accepted from " + 
+						cl.getInetAddress() +
+						"on port" +
+						cl.getPort());
+
+				dispatcher.addClient(cl);
+				pool.execute(new EchoThread(cl, dispatcher));
+				LOGGER.finest("Thread added to ThreadPool");
+
+			}
+		} catch (IOException ex) {
+			pool.shutdown();
+
+		}
+	}
+
+	public synchronized void  shutDown(){
+		LOGGER.info("Server is shutting down");
+		LOGGER.finest("Shutting down ThreadPool");
+		pool.shutdown();
+		LOGGER.finest("Shutting down ServerSocket");
+		try {
 			serverSocket.close();
 		} catch (IOException e) {
 			LOGGER.severe("Cannot close ServerSocket" + e.getMessage());
-	
+
 		}
-		   
-	   }
-	 }
+
+	}
+}
 
